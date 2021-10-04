7f4769cee76108bfe2ca23afb0dd6a1b85d1fcaa
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-10-04 20:44:03 +0600

    icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Footer() {
-  const [value, setValue] = React.useState(0);
-
   return (
     <Box sx={{ width: '100%' }}>
+
       <BottomNavigation
-      sx={{ width: '100%', backgroundColor:'black', color:'white' }}
+      sx={{ width: '100%', backgroundColor:'black', color:'white' , display:'flex', flexDirection:'column'}}
         >
       <p>This is just a React demo page</p>
+      <Box>
+      <FacebookIcon/>
+      <TwitterIcon/>
+      <GitHubIcon/>
+      </Box>
+    
+    
       </BottomNavigation>
     </Box>
   );
